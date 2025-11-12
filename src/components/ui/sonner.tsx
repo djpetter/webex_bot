7eef6a4e8887ed563d8c0fa4b549cc72301f3a7b
djpetter <petmortv@cisco.com>
2025-11-12 23:44:39 +0100

    Update sonner.tsx

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,25 +1,13 @@
 "use client";
 
-import { useTheme } from "next-themes@0.4.6";
-import { Toaster as Sonner, ToasterProps } from "sonner@2.0.3";
+import { Toaster as Sonner, type ToasterProps } from "sonner";
 
-const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
-
+export function Toaster(props: ToasterProps) {
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme="dark"
       className="toaster group"
-      style={
-        {
-          "--normal-bg": "var(--popover)",
-          "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
-        } as React.CSSProperties
-      }
       {...props}
     />
   );
-};
-
-export { Toaster };
+}
